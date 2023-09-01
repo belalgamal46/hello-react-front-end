53046158a0527dcca16f2ecf60f2d8f3a824dc82
theabroadwolf <belalgamal00@gmail.com>
2023-09-01 18:15:23 +0300

    Update greeint messages component with redux and add some styles

--- src/components/greetingMessages/GreetingMessages.jsx
+++ src/components/greetingMessages/GreetingMessages.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './greeting.module.css';
+import { getRandomMessage } from '../../redux/greetingMessage/greetingApi';
 
 function GreetingMessages() {
-  const handleClick = () => {};
+  const dispatch = useDispatch();
+  const [successMessage, setSuccessMessage] = useState(false);
+  const { greetingMessage, error } = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(getRandomMessage());
+  }, [dispatch]);
+
+  const handleClick = () => {
+    dispatch(getRandomMessage());
+    setSuccessMessage(true);
+
+    setTimeout(() => {
+      setSuccessMessage(false);
+    }, 500);
+  };
   return (
     <section id={styles['greeting-container']}>
-      <h1 className={styles['greeting-title']}>Greeting Message</h1>
+      {successMessage && <p className={styles['success-message']}>Successfully</p>}
+      {error.errorMessage && <p className={styles['error-message']}>{error.errorMessage}</p>}
+      <h1 className={styles['greeting-title']}>{greetingMessage.message}</h1>
       <button type="button" onClick={handleClick}>
         Greetings
       </button>
